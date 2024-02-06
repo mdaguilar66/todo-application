aa4ee14cac6cfb2eac25f4737695eb4c4ea59c08
Molly d'Aguilar <molly-daguilar@eastsideco.com>
2024-02-06 10:46:16 +0000

    pseudo code plan for js file

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Display Todos: Fetch Todo's from JSONPlaceholder API and display them
+
+    // Add Todo: Add Todo to list from the input field, send to JSONPlaceholder API (no page reload)
+
+    // Toggle Todo completion: mark Todo as complete, update status of todo in the JSONPlaceholder API
+
+    // Delete Todo: remove Todo item from list and from JSONPlaceholder API
+
+    // Error Handling: show error message for failed API requests
+})
